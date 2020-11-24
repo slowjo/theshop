5964690eb33ee1f8f211bfc91f492967742992c3
slowjo <fabiansen.s83@gmail.com>
2020-11-24 16:15:25 +0200

    not-found container removed

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,6 @@ import Layout from '../components/layout';
 
 export default () => (
     <Layout>
-        <div className="container py-2 not-found-container">
-            <h1>Sorry, page not found.</h1>
-        </div>
+        <h1>Sorry, page not found.</h1>
     </Layout>
 );
